128ed4c68e40a9fa97059c5f56a6480c74f5f449
Terosami <gregor@holzfeind.ch>
2019-11-21 10:13:38 +0100

    Update
    Anpassung Script

--- speedtest.sh
+++ speedtest.sh
@@ -2,24 +2,60 @@
 #Beschreibung
 #Speedtest mittels Curl und Iperf
 #by Gregor holzfeind <gregor@holzfeind.ch>
-#Version: 0.1
+#Version: 0.2.1
 #Datum: 2019.11.07
 
 #History
-#0.1	2019.11.07	Start des Scripts
+#0.1.0	2019.11.07	Start des Scripts
+#0.2.0	2019.11.14	Einbau Getops
+#0.2.1	2019.11.21	Arbeiten an dem Intaktiven Teil
 
 #Fixe Variablen
-version="0.1"
-whiptail_backtitle="Speedtest v$version"
+version="0.2.1"
+whiptail_backtitle="Speedtest V.$version"
+
+#Startvarablen
+speed_source=""
 
 
 #Funktionen
 
-function func_interaktiv() { #Funktion Intaktive
+function func_interactiv() { #Funktion Intaktive
 	whiptail \
 	--msgbox "Willkommen bei Speedtest\n\nDies ist die Intaktive Führung" \
 	--backtitle "$whiptail_backtitle" \
 	9 35 #Einleitung
+	func_interactiv_source
+}
+function func_interactiv_source() {
+	touch file_source.tmp.$$
+	while $source = true
+	do
+		input=$(whiptail \
+		--inputbox "Quelle für den Test" \
+		--backtitle "$whiptail_backtitle" \
+		$speed_source \
+		9 35 3>&1 1>&2 2>&3)
+		exitstatus=$?
+		if [ $exitstatus = 0 ]
+		then
+			func_input_check
+		else
+			func_canel
+		fi
+	done
+}
+
+function func_input_check() { #Funktion Input Überprüfung
+	whiptail \
+	--yesno "Stimmt diese Eingabe:\n\n$input" \
+	--backtitle "$whiptail_backtitle" \
+	12 35
+	exitstatus=$?
+	if [ $exitstatus = 0 ]
+	then
+		func_interactiv_more
+	fi
 }
 
 function func_speedtest() { #Funktion Speedtest
@@ -41,7 +77,6 @@ function func_time(){ #Funktion Zeitberechnung
 
 
 func_config() {
-	config="$zusatz1"
 	if [ ! -f "$config" ]
 	then
 		whiptail \
@@ -55,6 +90,7 @@ func_config() {
 function func_help() { #Hilfsfunktion
 	echo "Usage: speedtest.sh [OPTION]"
 	echo "Tool für einen Speedtest"
+	exit
 }
 #Programm
 OPTS=`getopt -o c:his:r: --long config:,help,interactiv,source:,rounds:  -n 'speedtest' -- "$@"`
@@ -65,7 +101,8 @@ echo "$OPTS"
 eval set -- "$OPTS"
 
 
-while true; do
+while true
+do
   case "$1" in
     -c | --config ) 
 		config_file="$2"
@@ -74,7 +111,7 @@ while true; do
 		func_help
 		;;
     -i | --interactiv )
-		func_interaktiv
+		func_interactiv
 		;;
     -s | --source ) 
 		$file_source="$2"
